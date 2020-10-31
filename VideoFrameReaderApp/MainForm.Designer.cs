4da7df2154fe43b525be870ab02d22f1691f0fa5
Rafiul Islam Shohan <ris.shohan@outlook.com>
2020-10-31 20:09:52 +0600

    Add project files.

---
+++ VideoFrameReaderApp/MainForm.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace VideoFrameReaderApp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnUpload = new System.Windows.Forms.Button();
+            this.txtSource = new System.Windows.Forms.TextBox();
+            this.btnGenerateFrame = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // btnUpload
+            // 
+            this.btnUpload.Location = new System.Drawing.Point(40, 32);
+            this.btnUpload.Name = "btnUpload";
+            this.btnUpload.Size = new System.Drawing.Size(116, 23);
+            this.btnUpload.TabIndex = 0;
+            this.btnUpload.Text = "Upload Video";
+            this.btnUpload.UseVisualStyleBackColor = true;
+            this.btnUpload.Click += new System.EventHandler(this.btnUpload_Click);
+            // 
+            // txtSource
+            // 
+            this.txtSource.Enabled = false;
+            this.txtSource.Location = new System.Drawing.Point(175, 31);
+            this.txtSource.Name = "txtSource";
+            this.txtSource.Size = new System.Drawing.Size(336, 23);
+            this.txtSource.TabIndex = 1;
+            // 
+            // btnGenerateFrame
+            // 
+            this.btnGenerateFrame.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.btnGenerateFrame.Location = new System.Drawing.Point(279, 125);
+            this.btnGenerateFrame.Name = "btnGenerateFrame";
+            this.btnGenerateFrame.Size = new System.Drawing.Size(116, 23);
+            this.btnGenerateFrame.TabIndex = 2;
+            this.btnGenerateFrame.Text = "Generate Frame";
+            this.btnGenerateFrame.UseVisualStyleBackColor = false;
+            this.btnGenerateFrame.Click += new System.EventHandler(this.btnGenerateFrame_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(40, 172);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(471, 16);
+            this.progressBar.TabIndex = 3;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(554, 450);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.btnGenerateFrame);
+            this.Controls.Add(this.txtSource);
+            this.Controls.Add(this.btnUpload);
+            this.Name = "MainForm";
+            this.Text = "Video Frame Reader";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnUpload;
+        private System.Windows.Forms.TextBox txtSource;
+        private System.Windows.Forms.Button btnGenerateFrame;
+        private System.Windows.Forms.ProgressBar progressBar;
+    }
+}
+
---
+++ VideoFrameReaderApp/MainForm.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using GleamTech.VideoUltimate;
+
+namespace VideoFrameReaderApp
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            
+        }
+        OpenFileDialog ofd = new OpenFileDialog();
+        private void btnUpload_Click(object sender, EventArgs e)
+        {
+            if(ofd.ShowDialog()== System.Windows.Forms.DialogResult.OK)
+            {
+                txtSource.Text = ofd.FileName;
+            }
+        }
+        private void btnGenerateFrame_Click(object sender, EventArgs e)
+        {
+         
+            //string path = @"F:\Grapics\video.mp4";
+      
+            string path = txtSource.Text; //Get video path from source path
+            using (var videoFrameReader = new VideoFrameReader(path))
+            {
+                if (videoFrameReader.Read()) //Only if frame was read successfully
+                {
+                    var resultCount = videoFrameReader.FrameRate * videoFrameReader.Duration.TotalSeconds;
+
+                    progressBar.Maximum = (int)resultCount; // set maximum value of progress bar
+                    
+                    int i = 1;
+                    foreach (var frame in videoFrameReader)
+                    {
+                        progressBar.Value += 1; // increement progess bar value
+                        using (frame) //save generated frame
+                        {
+                            frame.Save(@$"F:\Grapics\Frame\Frame{i}.jpg", ImageFormat.Jpeg);
+                            i++;
+                        }
+                    }
+
+                    progressBar.Value = 0; // reset progress bar
+                }
+            }
+        }
+    }
+}
